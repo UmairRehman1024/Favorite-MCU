5e2e856dd13b2ea88d3ff3cd2b7aeac140923de0
Umair Rehman <umairrehman66@gmail.com>
2022-11-19 13:58:43 +0000

    change a tag for link

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -4,16 +4,17 @@ import { IoSunnyOutline, IoMoonOutline } from 'react-icons/io5';
 import { Menu, Transition } from '@headlessui/react'
 import { Fragment, useEffect, useRef, useState } from 'react'
 //import { ChevronDownIcon } from '@heroicons/react/20/solid'
+import Link from 'next/link'; 
 
 
 const Navbar = ()  => {
     return(
     <nav className="bg-white px-2 sm:px-4 py-2.5 dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
      <div className="container flex flex-wrap items-center justify-between mx-auto">
-      <a href="/" className="flex items-center">
+      <Link href="/" className="flex items-center">
         <img src="https://ik.imagekit.io/9m4vpfigvq/MCU_Logo.webp?ik-sdk-version=javascript-1.4.3&updatedAt=1668706707989" className="h-6 mr-3 sm:h-9" alt="MCU Logo"></img>
         <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Favourite Mcu Movie</span>
-      </a>
+      </Link>
       <div className="flex md:order-2">
         <DarkModeToggle></DarkModeToggle>
         {/*}
@@ -30,10 +31,10 @@ const Navbar = ()  => {
      <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
         <ul className="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
           <li>
-            <a href="/" className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white" aria-current="page">Home</a>
+            <Link href="/" className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white" aria-current="page">Home</Link>
           </li>
           <li>
-            <a href="/results" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Results</a>
+            <Link href="/results" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Results</Link>
           </li>
         </ul>
       </div>
