0d8ee4a3a6d2ac4fd44f9417c3619685662fe876
Umair Rehman <umairrehman66@gmail.com>
2023-01-26 23:03:48 +0000

    fixed rankings overlap with navbar

--- src/pages/results.tsx
+++ src/pages/results.tsx
@@ -73,15 +73,13 @@ const ResultRow:React.FC<{
 
     return (
         <div className="relative flex border-b p-2 items-center justify-between">
-          <div className="flex items-center">
-             <div className="flex items-center pl-4">
-                <div className="pl-2 capitalize">{props.Movie.name}</div>
+            <div className="flex items-center pl-6">
+               <div className="pl-2 capitalize">{props.Movie.name}</div>
             </div>
-          </div>
           <div className="pr-4">
             {generateCountPercent(props.Movie).toFixed(2) + "%"}
           </div>
-          <div className="absolute top-0 left-0 z-20 flex items-center justify-center px-2 font-semibold text-white bg-gray-600 border border-gray-500 shadow-lg rounded-br-md">
+          <div className="absolute top-0 left-0 z-10 flex items-center justify-center px-2 font-semibold text-white bg-gray-600 border border-gray-500 shadow-lg rounded-br-md w-7">
             {props.rank}
           </div>
         </div>
